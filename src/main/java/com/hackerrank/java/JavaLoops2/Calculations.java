7521e5f35a887491a6115d99400c57e292bd9409
ahmetdagtas <a.dagtas@gmail.com>
2024-09-26 13:05:26 +0300

    TDD, the faulty result is generated test access modifier is changed

--- src/main/java/com/hackerrank/java/JavaLoops2/Calculations.java
+++ src/main/java/com/hackerrank/java/JavaLoops2/Calculations.java
@@ -2,4 +2,7 @@ package com.hackerrank.java.JavaLoops2;
 
 public class Calculations {
 
+    public static int getSecondPower(int i) {
+        return -1;
+    }
 }
--- src/test/java/com/hackerrank/java/TestCalculations.java
+++ src/test/java/com/hackerrank/java/TestCalculations.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class TestCalculations {
 
     @Test
-    private void TestSecondPower(){
+    public void TestSecondPower(){
         int result = Calculations.getSecondPower(0);
         Assert.assertEquals(result, 1);
 
