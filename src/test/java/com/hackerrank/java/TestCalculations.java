a0e09e5ef1ac31b15e76743daf607e75532dacaa
ahmetdagtas <a.dagtas@gmail.com>
2024-09-26 13:02:00 +0300

    TDD, no method generated

--- src/test/java/com/hackerrank/java/TestCalculations.java
+++ src/test/java/com/hackerrank/java/TestCalculations.java
@@ -1,11 +1,14 @@
 package com.hackerrank.java;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestCalculations {
 
     @Test
     private void TestSecondPower(){
+        int result = Calculations.getSecondPower(0);
+        Assert.assertEquals(result, 1);
 
     }
 }
