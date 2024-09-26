f81fa6fb49c8d7bd0ab3b34e5a0f2e00680e665f
ahmetdagtas <a.dagtas@gmail.com>
2024-09-26 13:06:36 +0300

    Test passed

--- src/main/java/com/hackerrank/java/JavaLoops2/Calculations.java
+++ src/main/java/com/hackerrank/java/JavaLoops2/Calculations.java
@@ -3,6 +3,6 @@ package com.hackerrank.java.JavaLoops2;
 public class Calculations {
 
     public static int getSecondPower(int i) {
-        return -1;
+        return (int)Math.pow(2, i);
     }
 }
